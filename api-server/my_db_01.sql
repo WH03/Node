34b6fd2ed3fca727db2281ebc91a3ad2c27661c0
w <847666794www@163.com>
2026-04-02 22:45:05 +0800

    fix：密码加密；向数据库插入数据；

--- api-server/my_db_01.sql
+++ api-server/my_db_01.sql
@@ -1,1 +1,86 @@
-SELECT * FROM my_db_01.ev_users;
+CREATE DATABASE  IF NOT EXISTS `my_db_01` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `my_db_01`;
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: my_db_01
+-- ------------------------------------------------------
+-- Server version	8.0.45
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ev_users`
+--
+
+DROP TABLE IF EXISTS `ev_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ev_users` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `username` varchar(255) NOT NULL COMMENT '用户名',
+  `password` varchar(255) NOT NULL COMMENT '密码',
+  `nickname` varchar(255) DEFAULT '' COMMENT '昵称',
+  `email` varchar(255) DEFAULT '' COMMENT '邮箱',
+  `user_pic` text COMMENT '头像',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COMMENT='用户数据表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ev_users`
+--
+
+LOCK TABLES `ev_users` WRITE;
+/*!40000 ALTER TABLE `ev_users` DISABLE KEYS */;
+INSERT INTO `ev_users` VALUES (1,'admin','000000','','',NULL),(2,'zs','$2a$10$DAgISnUpwL2jeW8VgKDFmuMgN/19pDfUzfkINnD/aBB.bGS37rDmW','','',NULL);
+/*!40000 ALTER TABLE `ev_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL COMMENT '用户登录名',
+  `password` varchar(45) NOT NULL COMMENT '用户登录密码',
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '用户状态，是一个布尔值\n0表示用户状态正常\n1表示用户被禁用',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户信息表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'zs','123456',0),(2,'ls','admin123',1),(3,'xh','654321',0),(4,'tony stark','888888',0),(6,'aaaa1','0000',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-02 22:44:07
--- api-server/router_handler/user.js
+++ api-server/router_handler/user.js
@@ -1,6 +1,9 @@
 // 导入数据库操作模块
 const db = require("../db/index");
 
+// 导入bcryptjs这个包
+const bcrypt = require('bcryptjs')
+
 // 注册新用户的处理函数
 exports.regUser = (req, res) => {
   // 获取客户端提交到服务器的用户信息
@@ -30,7 +33,19 @@ exports.regUser = (req, res) => {
     }
 
     // TODO: 用户名可用，继续后续操作...
-    console.log('添加成功')
+    // 调用bcrypt.hashSync()对密码进行加密
+    userinfo.password = bcrypt.hashSync(userinfo.password, 10)
+
+    // 定义插入新用户的SQL语句
+    const sql = 'insert into ev_users set ?'
+    db.query(sql, { username: userinfo.username, password: userinfo.password }, (err, results) => {
+      // 判断SQL语句是否执行成功
+      if (err) return res.send({ status: 1, message: err.message })
+      // 判断影响行数是否为1
+      if (results.affectedRows !== 1) return res.send({ status: 1, message: '注册用户失败，请稍后再试！' })
+      // 注册成功
+      res.send({ status: 0, message: '注册成功！' })
+    })
   });
 
   // res.send("reguser ok");
