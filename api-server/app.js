2c9fbca14718eb484a3ade66da2a6a6c3a75caca
w <847666794www@163.com>
2026-04-12 23:05:31 +0800

    fix: 封装返回函数

--- api-server/app.js
+++ api-server/app.js
@@ -11,6 +11,20 @@ app.use(cors());
 // 配置解析表单数据的中间件。注意：这个中间件，只能解析application/x-www-form-urlencoded格式的表单数据
 app.use(express.urlencoded({ extended: false }));
 
+// 一定要在路由之前，封装res.cc这个函数
+app.use((req, res, next) => {
+  // status默认值为1，表示失败的情况
+  // err的值,可能是一个错误对象,也可能是一个错误的,描述对象
+  res.cc = function (err, status = 1) {
+    res.send({
+      status,
+      message: err instanceof Error ? err.message : err,
+    })
+  }
+  next()
+})
+
+
 // 导入并使用路由模块
 const userRouter = require("./router/user");
 app.use("/api", userRouter);
--- api-server/router_handler/user.js
+++ api-server/router_handler/user.js
@@ -23,13 +23,15 @@ exports.regUser = (req, res) => {
     // 执行sql语句失败
     if (err) {
       return res.send({ status: 1, message: err.message });
+      // return res.cc(err)
     }
     // 判断用户名是否被占用
     if (results.length > 0) {
-      return res.send({
-        status: 1,
-        message: "用户名被占用，请更换其他用户名！",
-      });
+      // return res.send({
+      //   status: 1,
+      //   message: "用户名被占用，请更换其他用户名！",
+      // });
+      return res.cc('用户名被占用，请更换其他用户名！')
     }
 
     // TODO: 用户名可用，继续后续操作...
@@ -40,11 +42,13 @@ exports.regUser = (req, res) => {
     const sql = 'insert into ev_users set ?'
     db.query(sql, { username: userinfo.username, password: userinfo.password }, (err, results) => {
       // 判断SQL语句是否执行成功
-      if (err) return res.send({ status: 1, message: err.message })
+      if (err) return res.cc(err)
+
       // 判断影响行数是否为1
-      if (results.affectedRows !== 1) return res.send({ status: 1, message: '注册用户失败，请稍后再试！' })
+      if (results.affectedRows !== 1) return res.cc('注册用户失败，请稍后再试！')
       // 注册成功
-      res.send({ status: 0, message: '注册成功！' })
+      // res.send({ status: 0, message: '注册成功！' })
+      return res.cc('注册成功！', 0)
     })
   });
 
